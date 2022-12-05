ab694f81be0f4fcc99e2b60a2450716f36c103b9
starmaid <nicholas.masso.14@gmail.com>
2022-12-05 00:09:13 -0800

    started trying to generalize splitaround when mimi told me splitOn exists

--- 5/main.hs
+++ 5/main.hs
@@ -3,13 +3,16 @@
 {-# HLINT ignore "Use infix" #-}
 import System.IO
 import Data.List
-
+import Data.List.Split (splitOn)
 
 main = do  
-    contents <- readFile "input"
+    contents <- readFile "input1"
     let items = lines contents
 
-    let question = splitArndThing items ""
+    print items
+    let question = splitArndThing "" items
+    print question
+    {-
     let cargo = map (dropWhile (==' ')) (transpose [ map (!! 1) xs | xs <- map (splitBy3Col) (init (head question))])
     print cargo
     let instString = map (odds . splitArndThingR ' ') 
@@ -17,7 +20,7 @@ main = do
     let insts = [ [read y :: Int | y <- x] | x <- instString]
     print $ foldl applyMove cargo insts
     print $ map (head) (foldl applyMove cargo insts)
-
+    -}  
 
 applyMove stk mv = [ newcol is | is <- [0..length stk - 1]]
     where   (take,leave) = splitAt numcrates (stk !! fromL)
@@ -39,26 +42,23 @@ odds _ = []
 splitBy3Col :: [a] -> [[a]]
 splitBy3Col xs
     | length xs == 3 = [xs]
-    | otherwise = head3:splitBy3Col tail3
-    where ys = splitArndIndex 3 xs
-          head3 = head ys
-          tail3 = head (tail ys)
+    | otherwise = head3 : splitBy3Col tail3
+    where ys = splitAt 3 xs
+          head3 = fst ys
+          tail3 = snd ys
     
-splitArndIndex :: Int -> [a] -> [[a]]
-splitArndIndex i xs = fixTuple (splitAt i xs)
 
 splitArndThingR :: Eq a => a -> [a] -> [[a]]
 splitArndThingR c xs
     | notElem c xs = [xs]
-    | otherwise = head3:splitArndThingR c tail3
-    where ys = splitArndIndex i xs
+    | otherwise = head' : splitArndThingR c tail'
+    where ys = splitAt i xs
           i = resolveMaybe (elemIndex c xs)
-          head3 = head ys
-          tail3 = head (tail ys)
+          head' = fst ys
+          tail' = snd ys
 
-
-splitArndThing :: Eq a => [a] -> a -> [[a]]
-splitArndThing xs c = fixTuple (splitAt (resolveMaybe (elemIndex c xs)) xs)
+splitArndThing :: Eq a => a -> [a] -> [[a]]
+splitArndThing c xs = fixTuple (splitAt (resolveMaybe (elemIndex c xs)) xs)
 
 resolveMaybe :: Num a => Maybe a -> a
 resolveMaybe Nothing = 0
