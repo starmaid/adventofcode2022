76f98cc12fa103a564f127edf5da1a9cbb9f2551
starmaid <nicholas.masso.14@gmail.com>
2022-12-05 01:53:26 -0800

    add haskell boilerplate

---
+++ 0/main.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
+{-# HLINT ignore "Use infix" #-}
+import System.IO
+import Data.List
+import Data.List.Split (splitOn)
+
+main = do  
+    contents <- readFile "input1"
+    let items = lines contents
+
+    print items
