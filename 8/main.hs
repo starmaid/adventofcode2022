1b4945491c58261d21aefc8662be45debd948720
starmaid <nicholas.masso.14@gmail.com>
2022-12-07 22:16:26 -0800

    ok getting better

--- 8/main.hs
+++ 8/main.hs
@@ -4,9 +4,57 @@
 import System.IO
 import Data.List
 import Data.List.Split (splitOn)
+import Data.Maybe (maybe, fromJust, fromMaybe)
+import Data.Function (on)
 
 main = do  
-    contents <- readFile "input1"
+    contents <- readFile "input"
     let items = lines contents
+    let rows = [[read [i] | i <- s] | s <-items]
+    let cols = transpose rows
+    --print rows
+    --print cols
+    --print $ [[isVisible rows cols i j | i <- [0..length cols-1]] | j <- [0..length rows-1]]
+    --print $ length (filter (True ==) [isVisible rows cols i j | i <- [0..length cols-1], j <- [0..length rows-1]])
+    -- 1870 is not the right answer. right answer for someone else
+    print $ maximum [countVisible rows cols i j | i <- [0..length cols-1], j <- [0..length rows-1]]
+    
+countVisible :: [[Int]] -> [[Int]] -> Int -> Int -> Int
+countVisible rows cols x y = 
+    product [
+        -- from left
+        length (takeWhile (< height) llist) + islEdge,
+        -- from right
+        length (takeWhile (< height) rlist) + isrEdge,
+        -- from up
+        length (takeWhile (< height) ulist) + isuEdge,
+        -- from down
+        length (takeWhile ( < height) dlist) + isdEdge
+    ]
+    where height = rows !! y !! x
+          llist = reverse (take x (rows !! y))
+          rlist = drop (x+1) (rows !! y)
+          ulist = reverse (take y (cols !! x))
+          dlist = drop (y+1) (cols !! x)
+          islEdge = if maximum' llist >= height then 1 else 0
+          isrEdge = if maximum' rlist >= height then 1 else 0
+          isuEdge = if maximum' ulist >= height then 1 else 0
+          isdEdge = if maximum' dlist >= height then 1 else 0
 
-    print items
+isVisible :: [[Int]] -> [[Int]] -> Int -> Int -> Bool
+isVisible rows cols x y = 
+    or [
+        -- from left
+        maximum' (take x (rows !! y)) < (rows !! y !! x),
+        -- from right
+        maximum' (drop (x+1) (rows !! y)) < (rows !! y) !! x,
+        -- from top
+        maximum' (take y (cols !! x)) < (rows !! y) !! x,
+        -- from bottom
+        maximum' (drop (y+1) (cols !! x)) < (rows !! y) !! x
+    ]
+
+maximum' :: [Int] -> Int
+maximum' list
+    | null list = -1
+    | otherwise = maximum list
